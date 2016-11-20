50c6ef3bffb6c6a6913cd688abc85a6ef986c3ad
Ek Indharameesup <eindhara@yahoo.com>
2016-11-20 14:12:18 -0500

    initial test

---
+++ SampleWebAPI/SampleWebAPI/BuzLogic.fsx
@@ -0,0 +1,5 @@
+﻿
+module TestInfo =
+
+    let getTime () =
+        System.DateTime.UtcNow.ToShortDateString()
---
+++ SampleWebAPI/SampleWebAPI/Test01.fsx
@@ -0,0 +1,24 @@
+﻿#r @"..\packages\Suave.1.1.3\lib\net40\Suave.dll"
+#load "BuzLogic.fsx"
+
+open Suave
+open Suave.Filters
+open Suave.Operators
+open Suave.Successful
+open BuzLogic
+
+let app =
+  choose
+    [ GET >=> choose
+        [ 
+            path "/hello" >=> OK "Hello GET";
+            path "/goodbye" >=> OK "Good bye GET" 
+            path "/gettime" >=> request (fun r -> OK (TestInfo.getTime()))
+        ]
+      POST >=> choose
+        [ path "/hello" >=> OK "Hello POST";
+          path "/goodbye" >=> OK "Good bye POST" ] ]
+
+startWebServer defaultConfig app
+
+
